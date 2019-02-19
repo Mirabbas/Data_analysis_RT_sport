524850bab0da41c4b6ed0d97da1f28d54a4d45b7
Mirabbas <bagirov_mirabbas@mail.ru>
2019-02-20 01:03:53 +0300

    Add files via upload

---
+++ main.m
@@ -0,0 +1,18 @@
+function main()
+ 
+  WordFileName='lalalala.doc'; 
+   CurDir=pwd;
+   FileSpec = fullfile(CurDir,WordFileName);
+   [ActXWord,WordHandle]=StartWord(FileSpec); 
+   
+figure;
+hold on
+plot(res_l);
+plot(result,'r');
+hold off
+title('Надув');
+xlabel('сек');ylabel('код');
+FigureIntoWord(ActXWord); 
+    
+CloseWord(ActXWord,WordHandle,FileSpec);
+end
